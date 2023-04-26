b56ea081b7b685d2905f9be8a1157874d3dc447d
MarianoGuevara <marianoguevara2005@gmail.com>
2023-04-25 23:10:02 -0300

    calculadora 1 y algo de estilo

--- pag_uno.js
+++ pag_uno.js
@@ -1,4 +1,11 @@
 function mostrar_f_a_c()
 {
-    alert("Holaa")
+    dato_uno = document.getElementById("dato_uno").value;
+    dato_uno = parseInt(dato_uno)
+
+    result = (dato_uno * (9/5)) + 32
+    
+    document.getElementById("dato_dos").value = result
+
+    // document.getElementById("dato_dos")
 }
